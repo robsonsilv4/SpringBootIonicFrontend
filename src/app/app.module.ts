77b87322ee58a533d145fa0c4f702eefeea0cf0a
robsonsilv4 <robsonsilv410@gmail.com>
2018-07-19 09:16:38 -0300

    Criando página de detalhes do produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ErrorInterceptorProvider } from "../interceptors/error.interceptor";
 import { AuthService } from "../services/auth.service";
 import { StorageService } from "../services/storage.service";
 import { ClienteService } from "../services/domain/cliente.service";
+import { ProdutoService } from "../services/domain/produto.service";
 
 @NgModule({
   declarations: [MyApp],
