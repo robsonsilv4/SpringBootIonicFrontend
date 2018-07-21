28cfea245834b50c88bf18b061f747e3e668aea2
robsonsilv4 <robsonsilv410@gmail.com>
2018-07-21 08:59:58 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -18,6 +18,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EndrecoDTO;
+  codPedido: string;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -57,11 +58,15 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot("CartPage");
   }
 
+  home() {
+    this.navCtrl.setRoot("CategoriasPage");
+  }
+
   checkout() {
     this.pedidoService.insert(this.pedido).subscribe(
       response => {
         this.cartService.createOrClearCart;
-        console.log(response.headers.get("location"));
+        this.codPedido = this.extractId(response.headers.get("location"));
       },
       error => {
         if (error.status == 403) {
@@ -69,5 +74,10 @@ export class OrderConfirmationPage {
         }
       }
     );
+  }
+
+  private extractId(location: string): string {
+    let position = location.lastIndexOf("/");
+    return location.substring(position + 1, location.length);
   }
 }
